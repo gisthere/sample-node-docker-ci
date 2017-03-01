b50dded55ad2dd4226ac2ce46a2546dc3946fc14
gisthere <gisthere@gmail.com>
2017-03-01 15:58:37 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ docker pull gisthere/sample-node:latest
 docker stop web || true
 docker rm web || true
 docker rmi gisthere/sample-node:current || true
-docker tag gisthere/sample-node:latest kostyaurysov/sample-node:current
+docker tag gisthere/sample-node:latest gisthere/sample-node:current
 docker run -d --restart always --name web -p 80:80 gisthere/sample-node:current
 EOF
